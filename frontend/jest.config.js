0939a91b3b842bd10985ea3393e253c80e906cd1
punzak <steph.punzak@gmail.com>
2026-06-09 14:09:14 -0400

    Add files via upload

---
+++ frontend/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+      diagnostics: false
+    }]
+  }
+};
